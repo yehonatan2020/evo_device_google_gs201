LineageOS/android_device_google_gs101

Add AiAi allowlist
* In Android 12, Android System Intelligence (com.google.android.as) uses
  some of the Private Compute Service (com.google.android.as.oss) services.
  The absence of these Allowlists will cause Android System Intelligence
  to fail to connect to the network and cause collapse.
* We don't need to extract google.xml to fix it, just extract the AiAi part

Change-Id: I446ae5d243cff134901094f82c90a0bbf3bb4381

lineage_common.mk
@@ -7,6 +7,10 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/gs101/overlay-lineage
 
+# AiAi Config
+PRODUCT_COPY_FILES += \
+    device/google/gs101/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
+
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \

yehonatan2020/evo_device_google_gs201

lineage_common.mk
@@ -7,6 +7,10 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-lineage
 
+# AiAi Config
+PRODUCT_COPY_FILES += \
+    device/google/gs201/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
+
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \